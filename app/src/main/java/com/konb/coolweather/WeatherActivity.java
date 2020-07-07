4eb3b9221e1cadbaf586a4b95dd9b018cb6e198f
konb-hui <2581881495@qq.com>
2020-07-07 15:55:09 +0800

    Add the function of automatic weather update in the background

--- app/src/main/java/com/konb/coolweather/WeatherActivity.java
+++ app/src/main/java/com/konb/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.konb.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.konb.coolweather.gson.Forecast;
 import com.konb.coolweather.gson.Weather;
+import com.konb.coolweather.service.AutoUpdateService;
 import com.konb.coolweather.util.HttpUtil;
 import com.konb.coolweather.util.Utility;
 
@@ -191,6 +193,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /*
